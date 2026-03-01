187b11121c72264705f4db25a4b4dbaa3598c618
J.O <jo@J.O>
2026-02-28 18:58:30 -0600

    FULLY WOrking non prettyied up version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Techshell.c
-A Csc 222 shell like implentaition
+A Csc 222 shell like implentaition Jordan Owens
+```sh
+gcc Techshell.sc -o Techshell.c
+./Techshell.c
+````
+
+
+
+It has a superman(); that runs commands ls pwd cat 
+
+
+It has a function called cd();
+That if cd ~
+
+
+
--- Techshell.c
+++ Techshell.c
@@ -11,30 +11,27 @@
 #include <fcntl.h> 
 
 char directory[PATH_MAX];
-char input[67];
+char input[INPUT_MAX];
 char *args[ARGS_MAX];
 //4. Forbidden functions include system()and popen()
     /// displays money sign plus what ever 
     // ~$ if they ` it goes to home and if
     // they make itso what ever direcotry your in it display is and what ever is in it 
     //prints wirking direcotry 
-    char *infile = NULL;
-    char *outfile = NULL;
+   
 void cd(char *args[]);
 void superman(char *args[]);
 void extractor(char *input, char *args[]);
-int execvp(const char *file, char *const argv[]);
 void PACKMAN(char *INNNY, char *OUTTY);
 
 pid_t pid;
 char *home;
-char *root;
-char *INNNY = NULL;
-char *OUTTY = NULL;
+char *INNNY;
+char *OUTTY;
 
   int main(void){
 char directory[PATH_MAX];
-char input[67];
+char input[INPUT_MAX];
 ///char *args[4];
 
 char *home = getenv ("HOME");
@@ -47,19 +44,22 @@ char *root = getenv("ROOT");
     chdir(home);
  }
 while(1){
+    INNNY = NULL;
+     OUTTY = NULL;
  
 char directory[PATH_MAX];
 
 if (getcwd(directory, sizeof(directory)) != NULL) {
-    printf("$ %s$ ", directory);
+printf("%s$ ", directory);  
             fflush(stdout);
 }
 
 // ok so i have to to stdin to read the leyobard store into array the fgets to read the string then 
 // this reads my keyboard
-fgets(input,67,stdin);
-extractor(input, args);
+  fgets(input, INPUT_MAX, stdin);
+  extractor(input, args);
 int check_FORPACK = 0;
+    if (args[0] == NULL) continue;
 
 for (int i = 0; args[i] != NULL; i++) {
     if (strcmp(args[i], "<") == 0 || strcmp(args[i], ">") == 0){
@@ -68,20 +68,17 @@ for (int i = 0; args[i] != NULL; i++) {
 }
 }
 
-if (strcmp(args[0], "exit") == 0) 
+if (strcmp(args[0], "exit") == 0) {
 
 exit(0);
+}
 
-char *INNNY = NULL;
-char *OUTTY = NULL;
 
 for (int i = 0; args[i] != NULL; i++) {
     if (strcmp(args[i], "<") == 0 && args[i+1]) 
-    
-    infile = args[i+1];
+    INNNY = args[i+1];
     if (strcmp(args[i], ">") == 0 && args[i+1]) 
-    
-    outfile = args[i+1];
+    OUTTY = args[i+1];
 }
 
 if (strcmp(args[0], "cd") == 0) {
@@ -223,14 +220,13 @@ home = getenv("HOME");
         if (home) chdir(home);    
         return;
 }
-if(args[1] && strcmp(args[1], "/") == 0){
+if(args[1] && strcmp(args[1], "/root") == 0){
     perror("cannot open directory /root: Permission denied");
 }
 
 //if the string 
 if (chdir(args[1]) != 0){
-        perror("cd");
-    }
+ fprintf(stderr, "Error %d (%s)\n", errno, strerror(errno));    }
  
 }
 
@@ -250,22 +246,23 @@ void PACKMAN(char *INNNY, char *OUTTY) {
 
 if (INNNY) {
         int IN = open(INNNY, O_RDONLY);
-        if(IN < 0) {
-            perror("");
-            exit(1);
-        dup2(IN, 0);
-        close(IN);
+        if(IN < 0) { perror(INNNY); exit(1); }  
+        dup2(IN, STDIN_FILENO);       
+         close(IN);
         }
 
-if (OUTTY != NULL) {
+if (OUTTY) {
 int OUT = open(OUTTY,O_CREAT | O_WRONLY | O_TRUNC,0664);
- dup2(OUT,1);
-close(OUT);
+if (OUT < 0) { perror(OUTTY); exit(1); }    
+        dup2(OUT, STDOUT_FILENO);
+        close(OUT);
+
 }
+
 }
 
 
-}
+
 
 
 
